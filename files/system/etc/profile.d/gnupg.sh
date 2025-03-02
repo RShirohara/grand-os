e3a1114b46bc9bfce1ecc14bb1f28d7828511f3b
Ray Shirohara <RShirohara@proton.me>
2025-03-02 17:57:39 +0900

    fix: issue where Git running within VS Code cannot connect to GnuPG
    Fixes an issue where dynamically generated Socket Dir cannot be found
    by generating a Systemd unit file when the user logs in.

---
+++ files/system/etc/profile.d/gnupg.sh
@@ -0,0 +1,14 @@
+#/usr/bin/env bash
+
+export GNUPGHOME="${XDG_DATA_HOME:-${HOME}/.local/share}/gnupg"
+export GPG_SOCKET_DIR=$(gpgconf --list-dirs socketdir)
+
+find /usr/share/gnupg/systemd-unit-template -type "f" \
+| while read -r PATH; do \
+    FILE_NAME="${PATH##.*/}"
+    SYSTEMD_UNIT_DIR="${XDG_DATA_HOME:-${HOME}/.local/share}/systemd/user"
+    /usr/bin/mkdir --parents "${SYSTEMD_UNIT_DIR}"
+    /usr/bin/sed --expression="s|\${GPG_SOCKET_DIR}|${GPG_SOCKET_DIR}|g" \
+      "${PATH}" \
+      > "${SYSTEMD_UNIT_DIR}/${FILE_NAME}"
+  done
