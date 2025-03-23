1fe34e71c41c84908a7eeabc6c8d57db6e60c8b8
Ray Shirohara <RShirohara@proton.me>
2025-03-23 16:25:36 +0900

    feat: add flatpak overrides and integrations (#104)

--- files/system/etc/profile.d/gnupg.sh
+++ files/system/etc/profile.d/gnupg.sh
@@ -3,7 +3,7 @@
 export GNUPGHOME="${XDG_DATA_HOME:-${HOME}/.local/share}/gnupg"
 export GPG_SOCKET_DIR=$(gpgconf --list-dirs socketdir)
 
-find /usr/share/gnupg/systemd-unit-template -type "f" \
+find /etc/grand-os/templates/gnupg -type "f" \
 | while read -r PATH; do \
     FILE_NAME="${PATH##*/}"
     SYSTEMD_UNIT_DIR="${XDG_DATA_HOME:-${HOME}/.local/share}/systemd/user"
