82545f8a2b18e8af1d9c48eba9d7f77c91e1faf4
Ray Shirohara <RShirohara@proton.me>
2025-11-02 16:46:03 +0900

    refactor: replace `add-image-info.sh` to `update-os-release.sh`

---
+++ files/scripts/update-os-release.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -o "errexit" -o "nounset" -o "pipefail"
+
+# shellcheck source=/dev/null
+source /usr/lib/os-release
+
+NAME="GrandOS"
+ID="grand-os"
+RELEASE_VERSION="20251101.0"
+VERSION="${VERSION_ID}.${RELEASE_VERSION}"
+REPO_URL="https://github.com/RShirohara/grand-os"
+REPO_ISSUE_URL="https://github.com/RShirohara/grand-os/issues"
+VARIANT_ID="${IMAGE_NAME##*/}"
+
+cat << EOF > /usr/lib/os-release
+NAME="${NAME}"
+ID="${ID}"
+ID_LIKE="fedora"
+VERSION="${VERSION}"
+VERSION_ID="${VERSION_ID}"
+VERSION_CODENAME="${VERSION_CODENAME}"
+PLATFORM_ID="${PLATFORM_ID}"
+PRETTY_NAME="${NAME} ${VERSION}"
+ANSI_COLOR="${ANSI_COLOR}"
+LOGO="${LOGO}"
+CPE_NAME="cpe:/o/rshirohara/${ID}/${VERSION}"
+DEFAULT_HOSTNAME="${ID}"
+HOME_URL="${REPO_URL}"
+DOCUMENTATION_URL="${REPO_URL}"
+SUPPORT_URL="${REPO_ISSUE_URL}"
+BUG_REPORT_URL="${REPO_ISSUE_URL}"
+VARIANT="${VARIANT}"
+VARIANT_ID="${VARIANT_ID}"
+OSTREE_VERSION="${VERSION}"
+EOF
+
+cat /usr/lib/os-release
